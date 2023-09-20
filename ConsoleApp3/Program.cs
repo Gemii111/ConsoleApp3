48e34fc3ac610b4cc746b539ed4c195f65465e38
عمرو عفت ابراهيم احمد مصطفى <Amreffat@std.mans.edu.eg>
2023-09-20 16:28:10 +0300

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //static bool ()
+            
+            int num = 7;
+           
+               
+            for (int i = 2; i<= num; i++)
+            {
+                if (num % i == 0)
+                {
+                    Console.WriteLine("not prime");
+                }
+                else { Console.WriteLine("is prime"); }
+                     
+            }
+            
+
+
+
+
+
+        }
+    }
+}
